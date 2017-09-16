7242ce69c73bc1d22c6ca15efc2cd689555dac0c
maaz <maazrk96@gmail.com>
2017-09-16 23:46:38 +0530

    Cleaned the code

--- spamSMSClassification.py
+++ spamSMSClassification.py
@@ -1,64 +1,64 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[3]:
 
 
 import pandas as pd
 messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t', names=['labels', 'messages'])
 
 
-# In[9]:
+# In[4]:
 
 
 messages.head()
 
 
-# In[10]:
+# In[5]:
 
 
 messages.describe()
 
 
-# In[11]:
+# In[6]:
 
 
 messages.groupby('labels').describe()
 
 
-# In[12]:
+# In[7]:
 
 
 messages.head()
     
 
 
-# In[13]:
+# In[8]:
 
 
 messages['length'] = messages['messages'].apply(len)
 
 
-# In[14]:
+# In[9]:
 
 
 # messages.groupby('messages').describe()
 
 
-# In[15]:
+# In[10]:
 
 
 messages.head()
 
 
-# In[16]:
+# In[11]:
 
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# In[17]:
+# In[12]:
 
 
 get_ipython().magic('matplotlib inline')
@@ -66,19 +66,19 @@ get_ipython().magic('matplotlib inline')
 messages['length'].plot.hist(bins=50)
 
 
-# In[18]:
+# In[13]:
 
 
 messages['length'].describe()
 
 
-# In[19]:
+# In[14]:
 
 
 messages[messages['length'] == 910 ]['messages'].iloc[0]
 
 
-# In[20]:
+# In[15]:
 
 
 messages.hist(column='length', by='labels', bins=150, figsize=(12, 4))
@@ -87,69 +87,69 @@ messages.hist(column='length', by='labels', bins=150, figsize=(12, 4))
 # Y axis shows frequency of sms of that size
 
 
-# In[21]:
+# In[16]:
 
 
 import string
 
 
-# In[22]:
+# In[17]:
 
 
 list(string.punctuation)
 
 
-# In[23]:
+# In[18]:
 
 
-punc = '#This string is full -of crap and spam!! DaYM how do I handle this?'
+punc = '#This string is full -of stuff and spam!! DaYM how do I handle this?'
 
 
-# In[24]:
+# In[19]:
 
 
 nopunc = [c for c in punc if c not in string.punctuation]
 nopunc
 
 
-# In[25]:
+# In[20]:
 
 
 ''.join(nopunc)
 
 
-# In[26]:
+# In[21]:
 
 
 from nltk.corpus import stopwords
 
 
-# In[27]:
+# In[22]:
 
 
 print(stopwords.words('english'))
 
 
-# In[28]:
+# In[23]:
 
 
 nopunc = ''.join(nopunc).split()
 nopunc
 
 
-# In[29]:
+# In[24]:
 
 
 clean_text = [word for word in nopunc if word.lower() not in stopwords.words('english')]
 
 
-# In[30]:
+# In[25]:
 
 
 clean_text
 
 
-# In[31]:
+# In[26]:
 
 
 def text_cleaner(mess):
@@ -169,40 +169,39 @@ def text_cleaner(mess):
     
 
 
-# In[32]:
+# In[27]:
 
 
 messages.head()
 
 
-# In[33]:
+# In[28]:
 
 
 messages['messages'].head(5).apply(text_cleaner)
 
 
-# In[34]:
+# In[29]:
 
 
 from sklearn.feature_extraction.text import CountVectorizer #It returns a vocabulary of words,
 #then transform function is used to create a CountVector for every message 
 
 
-# In[35]:
+# In[30]:
 
 
 #Here we are providing the countvectorizer a clean list of words, cleaned using our text_cleaner function
 bow_transformer = CountVectorizer(analyzer=text_cleaner).fit(messages['messages'])
 
 
-# In[36]:
+# In[31]:
 
 
 print(len(bow_transformer.vocabulary_))
-bow_transformer.vocabulary_
 
 
-# In[37]:
+# In[32]:
 
 
 mess4 = messages['messages'][3]
@@ -211,14 +210,14 @@ print(mess4)
 
 # 
 
-# In[62]:
+# In[33]:
 
 
 bow4 = bow_transformer.transform([mess4])
 print(bow4)
 
 
-# In[39]:
+# In[34]:
 
 
 print(bow4.shape)
@@ -230,31 +229,31 @@ print(bow4.shape)
 
 
 
-# In[40]:
+# In[ ]:
 
 
 bow_transformer.get_feature_names()[9554]
 
 
-# In[41]:
+# In[ ]:
 
 
 messages_bow = bow_transformer.transform(messages['messages'])
 
 
-# In[42]:
+# In[ ]:
 
 
 print("Shape of Sparse Matrix", messages_bow.shape)
 
 
-# In[43]:
+# In[ ]:
 
 
 messages_bow.nnz #non-zero values
 
 
-# In[44]:
+# In[ ]:
 
 
 #Fullness
@@ -262,38 +261,38 @@ sparsity = (100.0 * (messages_bow.nnz/(messages_bow.shape[0]*messages_bow.shape[
 print("Sparsity: ", round(sparsity))
 
 
-# In[45]:
+# In[ ]:
 
 
 from sklearn.feature_extraction.text import TfidfTransformer
 
 
-# In[46]:
+# In[ ]:
 
 
 tfidf_transformer = TfidfTransformer().fit(messages_bow)
 
 
-# In[64]:
+# In[ ]:
 
 
 tfidf4 = tfidf_transformer.transform(bow4)
 
 
-# In[67]:
+# In[ ]:
 
 
 print(tfidf4)
 tfidf4.shape
 
 
-# In[69]:
+# In[ ]:
 
 
 tfidf_transformer.idf_[bow_transformer.vocabulary_['unconscious']]
 
 
-# In[89]:
+# In[ ]:
 
 
 messages_tfidf = tfidf_transformer.transform(messages_bow)
@@ -306,39 +305,39 @@ print(messages_tfidf.shape)
 #Training the model
 
 
-# In[51]:
+# In[ ]:
 
 
 from sklearn.naive_bayes import MultinomialNB
 
 
-# In[83]:
+# In[ ]:
 
 
 spam_detect_model = MultinomialNB().fit(messages_tfidf, messages['labels'])
 
 
-# In[88]:
+# In[ ]:
 
 
 print("Predicted label for 4th SMS: ",spam_detect_model.predict(tfidf4)[0])
 print("Actual label for 4th SMS: ",messages['labels'][3])
 
 
-# In[76]:
+# In[ ]:
 
 
 all_predictions = spam_detect_model.predict(messages_tfidf)
 print(all_predictions)
 
 
-# In[102]:
+# In[ ]:
 
 
 #Accuracy is 100% as we are testing on the same data on which we trained, so we need to split the dataset
 
 
-# In[104]:
+# In[ ]:
 
 
 #Evaluating the model
@@ -347,20 +346,20 @@ from sklearn.metrics import classification_report
 print(classification_report(messages['labels'], all_predictions))
 
 
-# In[106]:
+# In[ ]:
 
 
 from sklearn.cross_validation import train_test_split
 
 
-# In[93]:
+# In[ ]:
 
 
 msg_train, msg_test, label_train, label_test = train_test_split(messages['messages'], messages['labels'], test_size=0.3)
 print(len(msg_train), len(msg_test), len(msg_train)+ len(msg_test))
 
 
-# In[94]:
+# In[ ]:
 
 
 #Creating a pipeline as the steps to be followed stays the same
@@ -374,19 +373,19 @@ pipeline = Pipeline([
 ])
 
 
-# In[97]:
+# In[ ]:
 
 
 pipeline.fit(msg_train, label_train)
 
 
-# In[98]:
+# In[ ]:
 
 
 predictions = pipeline.predict(msg_test)
 
 
-# In[112]:
+# In[ ]:
 
 
 print(classification_report(predictions, label_test))
